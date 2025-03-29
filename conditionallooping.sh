ad0d6bb6e8f34ebe1e6ea94056ae8ee4f4e6fa57
sanjaraiy <deepakrai78788@gmail.com>
2025-03-29 11:23:48 +0530

    added if,else, elif

--- conditionallooping.sh
+++ conditionallooping.sh
@@ -6,12 +6,17 @@ This is infotaimnent purpose
 
 disclaimer
 
-read -p "Enter the age: " age
+read -p "Enter your gender: " gender
+read -p "Enter your age: " age
 
-if [[ $age -eq 18 ]]; then
+if [[ $gender == "male" ]]; then
+    echo "You are a boy"
+elif [[ $age -ge 18 ]]; then
     echo "You are a Teenager"
 else
     echo "You are not a Teenager"
 fi
+
+
 
 
